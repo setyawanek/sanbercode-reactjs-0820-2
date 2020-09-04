f293e2756a411758e757843bad4ed85939e4c555
setyawanek <seko6661@gmail.com>
2020-09-04 12:33:10 +0700

    menambahkan tugas 9 dan 10

--- src/App.js
+++ src/App.js
@@ -1,48 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import FormBuah from './tugas-9/tugas9'
+//import UserInfo from './tugas-10/Userinfo';
+import HargaBuah from './tugas-10/tugas10'
+
 
 function App() {
   return (
+      // <div>
+      //   <FormBuah/>
+      // </div>
 
-    <div style={{width: '50%', margin: '0 auto', border: '1px solid black', borderRadius: 20}}>
-      <h1 style={{textAlign: 'center'}}>Form Pembelian Buah</h1>
-        <div style={{padding: 20, paddingTop: 0}}>
-          <div style={{display: 'block', marginBottom: '1em'}}>
-            <div style={{display: 'inline-block', width: 150, fontWeight: 'bold', fontSize: 16}}>
-              Nama Pelanggan
-            </div>
-
-    <input style={{display: 'inline-block'}} type="text" name="name" />
-        <div style={{display: 'block', marginBottom: '1em'}}>
-           <div style={{display: 'inline-block', width: 150, fontWeight: 'bold', fontSize: 16}}>
-              Daftar Item 
-          </div>
-
-      <div style={{display: 'inline-block'}}>
-        <input type="checkbox" name="semangka" defaultValue="semangka" />
-        <label>Semangka</label><br />
-        <input type="checkbox" name="jeruk" defaultValue="jeruk" />
-        <label>Jeruk</label><br />
-        <input type="checkbox" name="nanas" defaultValue="nanas" />
-        <label>Nanas</label><br />
-        <input type="checkbox" name="salak" defaultValue="salak" />
-        <label>Salak</label><br />
-        <input type="checkbox" name="anggur" defaultValue="anggur" />
-        <label>Anggur</label><br />
+      <div>
+        <HargaBuah/>
       </div>
 
-      </div>
-        <button style={{background: 'white', borderRadius: 20}}>
-          <a href="#" style={{textDecoration: 'none', color: 'black'}}>Kirim</a>
-        </button>
-      </div>
-
-        </div>
-    </div>
-
-
+      // <div>
+      //   <UserInfo />
+      // </div>  
+    
   );
 }
 
 export default App;
+
+
